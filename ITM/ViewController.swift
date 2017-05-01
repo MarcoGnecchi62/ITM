cdce16a19b2a86655ed5c4940a9c2833c5b1f3a4
Marco Gnecchi <marcognecchi@marcos-imac.homenet.telecomitalia.it>
2017-05-01 21:01:54 +0200

    Initial Commit

---
+++ ITM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ITM
+//
+//  Created by Marco Gnecchi on 01/05/2017.
+//  Copyright © 2017 Marco Gnecchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
